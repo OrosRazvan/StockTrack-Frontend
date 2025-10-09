bd4f58673cc24c19926275662f4ff7fb4b173301
Fio <fio.iuhasz@gmail.com>
2025-10-10 00:18:02 +0300

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import Typography from "@mui/material/Typography";
+
+export const App = () => {
+  return (
+    <Typography variant="h5" color="secondary" sx={{ fontWeight: 600 }}>
+      This is an innitial commit message
+    </Typography>
+  );
+};
